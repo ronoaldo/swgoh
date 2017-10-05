34c2236cf9ab9c8855f8889bd6192bbb69286dc1
Ronoaldo JLP <ronoaldo@gmail.com>
2017-10-05 00:17:35 +0000

    Added LRU caching for API calls.

--- swgohgg/client.go
+++ swgohgg/client.go
@@ -1,15 +1,21 @@
 package swgohgg
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
+	"github.com/hashicorp/golang-lru"
 )
 
+var globalCache, _ = lru.New(1024)
+
 type Client struct {
-	hc      *http.Client
-	profile string
+	hc       *http.Client
+	profile  string
+	useCache bool
 }
 
 func NewClient(profile string) *Client {
@@ -20,6 +26,12 @@ func NewClient(profile string) *Client {
 }
 
 func (c *Client) Get(url string) (*goquery.Document, error) {
+	b, ok := globalCache.Get(url)
+	if ok && c.useCache {
+		// Already on cache, use it!
+		return goquery.NewDocumentFromReader(bytes.NewBuffer(b.([]byte)))
+	}
+	// Not in cache, fetch from remote site
 	resp, err := c.hc.Get(url)
 	if err != nil {
 		return nil, err
@@ -31,7 +43,11 @@ func (c *Client) Get(url string) (*goquery.Document, error) {
 	if resp.StatusCode != 200 {
 		return nil, fmt.Errorf("swgohgg: unexpected status code %d", resp.StatusCode)
 	}
-	return goquery.NewDocumentFromReader(resp.Body)
+	data, err := ioutil.ReadAll(resp.Body)
+	if c.useCache {
+		globalCache.Add(url, data)
+	}
+	return goquery.NewDocumentFromReader(bytes.NewBuffer(data))
 }
 
 func (c *Client) UseHTTPClient(hc *http.Client) *Client {
@@ -42,4 +58,13 @@ func (c *Client) UseHTTPClient(hc *http.Client) *Client {
 func (c *Client) Profile(profile string) *Client {
 	c.profile = profile
 	return c
+}
+
+func (c *Client) UseCache(useCache bool) *Client {
+	c.useCache = useCache
+	return c
+}
+
+func FlushCache() {
+	globalCache.Purge()
 }
