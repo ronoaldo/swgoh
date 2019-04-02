09998cb60c52bde1b266c65fa5d1fbbe9de5ae79
Ronoaldo JLP <ronoaldo@gmail.com>
2019-04-02 20:38:21 -0300

    Fixed build after refactor.

--- units.go
+++ units.go
@@ -275,8 +275,6 @@ func CharName(src string) string {
 		return "Sith Assassin"
 	case "strooper", "trooper", "sit":
 		return "Sith Trooper"
-	case "marauder":
-		return "Sith Marauder"
 	case "snow":
 		return "Snowtrooper"
 	case "st", "storm":
